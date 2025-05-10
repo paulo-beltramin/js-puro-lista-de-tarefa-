0764ba543f4a2ea7b84eddddcf88982b45391070
paulo-beltramin <dev.beltramin@gmail.com>
2025-05-10 18:34:19 -0300

    criado botao remover, editar, e excluir e função de renderizar as tarefas

--- index.js
+++ index.js
@@ -1,28 +1,94 @@
-
 let lista = []
 
 const addTask = () => {
+
     let task = document.getElementById('nome-tarefa')
-    let list = document.getElementById('list')
     let error = document.getElementById("error")
-    let elementLi = document.createElement('li')
     let msgError = "O campo ''digite sua tarefa'' é obrigatório..."
-    if (task.value === "") {
+
+
+
+    if (task.value == "") {
         error.textContent = msgError
         return
     } else {
-
         lista.push(task.value)
+        renderTask()
         error.textContent = ""
     }
 
-    list.appendChild(elementLi)
-
-    lista.map(item => {
-        elementLi.textContent = item
-        console.log(lista)
-    })
-
-    task.value = ''
+    task.value = ""
 
 }
+
+const renderTask = () => {
+    let list = document.getElementById("list")
+    list.innerHTML = ""
+
+    for (let i = 0; i < lista.length; i++) {
+        let li_1 = document.createElement('li')
+
+        li_1.textContent = lista[i]
+
+        let botaoRemover = document.createElement("button")
+        botaoRemover.classList = "botao-remover"
+        botaoRemover.textContent = "Remover"
+        li_1.appendChild(botaoRemover)
+        botaoRemover.onclick = () => removeTask(i)
+
+        let botaoEditar = document.createElement("button")
+        
+        botaoEditar.classList = "botao-editar"
+        botaoEditar.textContent = 'Editar'
+        li_1.appendChild(botaoEditar)
+        botaoEditar.onclick = () => editTask(i)
+
+        let botaoLimpar = document.createElement("button")
+        botaoLimpar.classList = "botao-limpar"
+        botaoLimpar.textContent = 'Limpar tarefas'
+        li_1.appendChild(botaoLimpar)
+        botaoLimpar.onclick = () => cleartasks(i)
+
+
+        list.appendChild(li_1)
+    }
+
+}
+
+const removeTask = (i) => {
+
+    lista.splice(i, 1)
+    renderTask()
+}
+
+const editTask = (id) => {
+    let editando = prompt('edite a tarefa')
+
+    if (editando !== " ") {
+        lista[id] = editando
+        renderTask()
+    }
+}
+
+const cleartasks = () => {
+    lista.length = 0
+    renderTask()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
